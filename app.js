9caba2d5f00849df5c9bb84148a626a18225abba
ntibbals <ntibbals@outlook.com>
2018-10-29 14:26:31 -0700

    completion of cust gen

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ var firstAndPike = {
   minCust: 23,
   maxCust: 65
   avgSale: 6.3,
-  
-}
+  custPerHour: function(minCust, maxCust0) {
+    return Math.floor(Math.random() * (this.minCust - this.maxCust) + this.minCust);
+  },
+  per () {
+    var cookiesPerHour = storeOne.custPerHour() * this.avgCookies;
+  }
+};
