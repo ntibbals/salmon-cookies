5957416a239f2ea261943891653bff2b4600ac10
ntibbals <ntibbals@outlook.com>
2018-10-29 17:10:35 -0700

    finished for loop on store one

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+var hoursOfOperation = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
 var firstAndPike = {
   name: '1\'st and Pike',
   minCust: 23,
   maxCust: 65,
   avgSale: 6.3,
-  custPerHour: function (minCust, maxCust) {
-    var randomCustomer =  Math.floor((Math.random() * (this.maxCust - this.minCust)) + this.minCust);
-    var cookiesPerHour = Math.floor(randomCustomer * this.avgSale); 
-    return [randomCustomer, cookiesPerHour];
-  },
+  custPerHour: function () {
+    var randomCustomer = Math.floor((Math.random() * (this.maxCust - this.minCust)) + this.minCust);
+    var cookiesPerHour = Math.floor(randomCustomer * this.avgSale);
+    for (var i = 0; i < hoursOfOperation.length; i++) {
+      console.log([hoursOfOperation[i], cookiesPerHour]);
+    }
+  }
 };
 firstAndPike.custPerHour();
