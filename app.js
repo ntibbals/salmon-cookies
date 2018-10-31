18039f55382f5b1356dbb98770d28746b0a6eeff
ntibbals <ntibbals@outlook.com>
2018-10-31 14:16:41 -0700

    Completed table without total rows per hour

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var stores = [];
-
-function Store(name, minCust, maxCust, avgSale) {
+var hoursOfOps = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
+  function Store(name, minCust, maxCust, avgSale) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -15,7 +15,7 @@ function Store(name, minCust, maxCust, avgSale) {
   // this.render();
 }
 Store.prototype.generateRandomCustPerHour = function () {
-  for (var i = 0; i < this.hoursOfOps.length; i++) {
+  for (var i = 0; i < hoursOfOps.length; i++) {
     var randomCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
     this.custPerHour.push(randomCust);
     console.log(randomCust);
@@ -24,7 +24,7 @@ Store.prototype.generateRandomCustPerHour = function () {
 Store.prototype.generateHourlySales = function () {
   //line below will popluate custPerHour Array
   this.generateRandomCustPerHour();
-  for (var i = 0; i < this.hoursOfOps.length; i++) {
+  for (var i = 0; i < hoursOfOps.length; i++) {
     var perHour = Math.round(this.custPerHour[i] * this.avgSale);
     this.cookiesPerHour.push(perHour);
     //this.dailyTotal= this.dailyTotal + perHour
@@ -37,41 +37,50 @@ Store.prototype.generateHourlySales = function () {
 Store.prototype.render = function () {
   // this.generateHourlySales();
   var tbodyEl = document.getElementById('tbl-body'); ///anchors to html page
-  var trEl = document.createElement('tr'); ///create table row element tag
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th'); ///create table row element tag
   tbodyEl.appendChild(trEl);
-  var thEl = document.createElement('th'); // create table head element tag
-  trEl.textContent = this.name;
-  // trEl.appendChild(thEl);
-  // thEl.textContent = this.hoursOfOps;
-  for (var i = 0; i < this.hoursOfOps.length; i++) {
-    // var hourEl = document.createElement('td');
+  thEl.textContent = this.name;
+  trEl.appendChild(thEl);
+  for (var i = 0; i < hoursOfOps.length; i++) {
     var tdEl = document.createElement('td');
     tdEl.textContent = this.cookiesPerHour[i];
     trEl.appendChild(tdEl);
-    var cookieEl = document.createElement('td');
-    // cookieEl.textContent = `${this.cookiesPerHour[i]}`;
-    // trEl.appendChild(cookieEl);
-    var dailySumEl = document.createElement('th1');
+
   }
-  // trEl.appendChild(dailySumEl);
-  // tbodyEl.appendChild(trEl);
-  // tbodyEl.appendChild(thEl);
+  var tdElTotals = document.createElement('td');
+  tdElTotals.textContent = this.dailyTotal;
+  trEl.appendChild(tdElTotals);
+  
+
+
 };
 
-// function createHead() {
-//   var tableHeaderElement = document.getElementById('thead');
+function createHead() {
+  var tableHeadElement = document.getElementById('tbl-head');
+  var trEl = document.createElement('tr');
+  var tdElTotals = document.createElement('td');
+  tdElTotals.textContent = 'Locations';
+  trEl.appendChild(tdElTotals);
+  tableHeadElement.appendChild(trEl);
+  for (var i = 0; i < hoursOfOps.length; i++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = this.hoursOfOps[i];
+    trEl.appendChild(thEl);
+  }
 
-// }
-// function createFoot() {
-
-// }
+  var thElTotalTitle = document.createElement('th');
+  
+  thElTotalTitle.textContent = 'Daily Totals';
+  trEl.appendChild(thElTotalTitle);
+}
 function createTable() {
   var mainEl = document.getElementById('pikestore');
   var tblEl = document.createElement('table');
   var theadEl = document.createElement('thead');
   var tbodyEl = document.createElement('tbody');
   var tfootEl = document.createElement('tfoot');
-    
+
   tblEl.id = 'store-table';
   theadEl.id = 'tbl-head';
   tbodyEl.id = 'tbl-body';
@@ -82,13 +91,17 @@ function createTable() {
   tblEl.appendChild(tbodyEl);
   tblEl.appendChild(tfootEl);
   // tbodyEl.appendChild(render)
-    
+
 }
 createTable();
+// createHead();
+
 console.log(createTable);
 new Store('1st and Pike', 23, 65, 6.3);
 new Store('SeaTac Airport', 3, 24, 1.2);
 new Store('Seattle Center', 11, 38, 3.7);
 new Store('Capitol Hill', 20, 38, 2.3);
 new Store('Alki', 2, 16, 4.6);
+createHead();
+
 
